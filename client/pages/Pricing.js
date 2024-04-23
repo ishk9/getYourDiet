9758fc94916018b6d51f3e629fcb5312e2642187
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-04-23 21:20:48 +0530

    Added Pricing page template!

---
+++ client/pages/Pricing.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function PricingPage() {
+    return (
+        <div>
+            <p className="text-white">Pricing page</p>
+        </div>
+    )
+}
+
