b6c8438c59c1cbfb2eaa96a07891d81e0d6e029b
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-04-24 12:09:56 +0530

    Shifted pages!

---
+++ client/app/Pricing/page.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+export default function PricingPage() {
+    return (
+        <div className="flex w-full h-full justify-center items-center bg-black">
+
+            <div className="flex flex-col w-1/2 justify-center items-center">
+            <h1 className="font-extrabold text-white text-[6em] leading-[1.2] tracking-[-2px]">YOU CAN EAT ANYTHING</h1>
+
+            <p className="mt-10">
+                Our app is designed to help you successfully create a diet based on your body and the food you like. Easily optimize your eating schedule and receive community support
+                to reach your goals.
+            </p>
+
+
+            <div className="flex flex-col w-full py-12">
+                <hr className="h-[1px] w-full bg-white"/>
+                
+                <div className="flex flex-row h-full justify-center items-center">
+                    <div className="flex flex-col w-1/2 justify-center items-center">
+                        <p>Recipes for all tastes</p>
+                    </div>
+
+
+                    <div className="flex flex-col h-full w-1/2 justify-center items-center">
+                        <p>Nutrition Programs for all purposes</p>
+                       
+                    </div>
+                </div>
+
+                
+            </div>
+
+            </div>
+            
+            <div className="flex w-1/2 ">
+
+            </div>
+
+        </div>
+    )
+}
