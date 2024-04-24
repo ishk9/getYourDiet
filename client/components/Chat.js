00e1dda6b6bace8c08a6838b22277d8952044de9
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-04-24 14:50:41 +0530

    Feature: Created Chat box!

---
+++ client/components/Chat.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Chat({props}) {
+    return (
+        <div className="container hover:bg-neutral-900  p-3 my-1 rounded-lg">
+            <div className="flex justify-between items-center">
+                {/* Name */}
+                <p className='text-[15px]'>{props.name}</p>
+                {/* Date */}
+                <p className='text-[12px]'>{props.date}</p>
+            </div>
+
+            {/* Description */}
+            <p className='text-[12px] mt-1'>{props.description}</p>
+        </div>
+    )
+}
+
+export default Chat
