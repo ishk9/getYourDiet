77b3195a19705993f34c89bef3cc826f5495e5e7
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-04-24 18:36:24 +0530

    Fix: Added backdrop!

--- client/components/History.js
+++ client/components/History.js
@@ -18,54 +18,70 @@ const dummyData = [
     {id: 6, name: "Khullar", date: '24 April', description: 'abcscwdvnovnefwdcf'},
     {id: 7, name: "Khullar", date: '24 April', description: 'abcscwdvnovnefwdcf'},
     {id: 8, name: "Khullar", date: '24 April', description: 'abcscwdvnovnefwdcf'},
+    {id: 8, name: "Khullar", date: '24 April', description: 'abcscwdvnovnefwdcf'},
+    {id: 8, name: "Khullar", date: '24 April', description: 'abcscwdvnovnefwdcf'},
+    {id: 8, name: "Khullar", date: '24 April', description: 'abcscwdvnovnefwdcf'},
+    {id: 8, name: "Khullar", date: '24 April', description: 'abcscwdvnovnefwdcf'},
+    {id: 8, name: "Khullar", date: '24 April', description: 'abcscwdvnovnefwdcf'},
+    {id: 8, name: "Khullar", date: '24 April', description: 'abcscwdvnovnefwdcf'},
+    {id: 8, name: "Khullar", date: '24 April', description: 'abcscwdvnovnefwdcf'},
+    {id: 8, name: "Khullar", date: '24 April', description: 'abcscwdvnovnefwdcf'},
+    {id: 8, name: "Khullar", date: '24 April', description: 'abcscwdvnovnefwdcf'},
+    {id: 8, name: "Khullar", date: '24 April', description: 'abcscwdvnovnefwdcf'},
+    {id: 8, name: "Khullar", date: '24 April', description: 'abcscwdvnovnefwdcf'},
+    {id: 8, name: "Khullar", date: '24 April', description: 'abcscwdvnovnefwdcf'},
+    {id: 8, name: "Khullar", date: '24 April', description: 'abcscwdvnovnefwdcf'},
+    {id: 8, name: "Khullar", date: '24 April', description: 'abcscwdvnovnefwdcf'},
+    {id: 8, name: "Khullar", date: '24 April', description: 'abcscwdvnovnefwdcf'},
+    {id: 8, name: "Khullar", date: '24 April', description: 'abcscwdvnovnefwdcf'},
 ]
 
 function History() {
     return (
-        <div className="w-1/5 p-5">
-            <div className='flex justify-center items-center'>
-                {/* Search Bar */}
-                <div className='flex'>
-                    <input className="h-9 rounded-l-lg w-full bg-neutral-900 p-2 outline-none" 
-                    />
-                    <button className="flex justify-center items-center hover:bg-neutral-800 h-9 w-8 bg-neutral-900 rounded-r-lg">
-                        <GoSearch />
+        <div className="w-1/5 px-5 overflow-auto">
+            {/* Search Bar */}
+            <div className='backdrop-filter backdrop-brightness-0 sticky top-0 z-10 py-5'>
+                <div className='flex justify-center items-center backdrop-filter backdrop-brightness-0'>
+                    <div className='flex'>
+                        <input className="h-9 rounded-l-lg w-full bg-neutral-900 p-2 outline-none" 
+                        />
+                        <button className="flex justify-center items-center hover:bg-neutral-800 h-9 w-8 bg-neutral-900 rounded-r-lg">
+                            <GoSearch />
+                        </button>
+                    </div>
+
+                    {/* Create new button */}
+                    <button className="flex justify-center items-center h-9 w-11 ml-2 bg-[#CBBCA5] rounded-lg">
+                        <FiEdit color='black' size={16}/>
                     </button>
                 </div>
 
-                {/* Create new button */}
-                <button className="flex justify-center items-center h-9 w-11 ml-2 bg-[#CBBCA5] rounded-lg">
-                    <FiEdit color='black' size={16}/>
-                </button>
+    
+                <div className='flex flex-col mt-5 sticky top-14'>
+                    <div className='flex justify-start items-start'>
+                        <BsPinAngle className='text-neutral-600'/>
+                        <p className='text-neutral-600 font-semibold text-[10px] uppercase ml-1'>Pinned</p>
+                    </div>
 
+
+                    {/* Pinned Diets */}
+                    <div className='flex flex-col mt-2'>
+                        {dummyDataPinned.map((data, id) => (
+                            <Chat key={id} props={data}/>
+                        ))}
+                    </div>
+
+                    <div className='flex justify-start items-start sticky top-64 z-10 backdrop-filter backdrop-brightness-0 mt-2'>
+                        <MdOutlineDataset className='text-neutral-600'/>
+                        <p className='text-neutral-600 font-semibold text-[10px] uppercase ml-1'>All</p>
+                    </div>
+
+                </div>
             </div>
 
-                
-
-            <div className='flex flex-col mt-5'>
-                <div className='flex justify-start items-start'>
-                    <BsPinAngle className='text-neutral-600'/>
-                    <p className='text-neutral-600 font-semibold text-[10px] uppercase ml-1'>Pinned</p>
-                </div>
 
 
-                {/* Pinned Diets */}
-                <div className='flex flex-col mt-2'>
-                    {dummyDataPinned.map((data, id) => (
-                        <Chat key={id} props={data}/>
-                    ))}
-                </div>
-
-            </div>
-
-
-            <div className='flex flex-col mt-5'>
-                <div className='flex justify-start items-start'>
-                    <MdOutlineDataset className='text-neutral-600'/>
-                    <p className='text-neutral-600 font-semibold text-[10px] uppercase ml-1'>All</p>
-                </div>
-
-
+            <div className='flex flex-col'>
                 {/* All Diets */}
                 <div className='flex flex-col mt-2'>
                     {dummyData.map((data, id) => (
