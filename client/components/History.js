ab0b6da55ea5ce9a00cd0a1cd9f872ea4d73cbe3
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-04-24 14:19:21 +0530

    Feature: Added Search Bar!

---
+++ client/components/History.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { FiEdit } from "react-icons/fi";
+import { GoSearch } from "react-icons/go";
+
+function History() {
+    return (
+        <div className="w-1/5 p-5">
+            <div className='flex justify-center items-center'>
+                {/* Search Bar */}
+                <div className='flex'>
+                    <input className="h-8 rounded-l-lg w-full bg-neutral-900 p-2 outline-none" 
+                    />
+                    <button className="flex justify-center items-center hover:bg-neutral-800 h-8 w-8 bg-neutral-900 rounded-r-lg">
+                        <GoSearch />
+                    </button>
+                </div>
+
+                {/* Create new button */}
+                <div className="flex justify-center items-center h-8 w-10 ml-2 bg-stone-100 rounded-lg">
+                    <FiEdit color='black' size={16}/>
+                </div>
+
+            </div>
+
+                
+
+            <div>
+                {/* Pinned Diets */}
+            </div>
+
+            <div>
+                {/* All Diets */}
+            </div>
+
+
+            <div>
+                {/* Clear all button */}
+            </div>
+
+        </div>
+    )
+}
+
+export default History
