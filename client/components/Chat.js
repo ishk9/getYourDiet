b27af12bbdf6699a3df1ea29d5376a087564759d
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-04-24 17:33:59 +0530

    Fix: Changed div to button

--- client/components/Chat.js
+++ client/components/Chat.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Chat({props}) {
     return (
-        <div className="container hover:bg-neutral-900  p-3 my-1 rounded-lg">
+        <button className="container hover:bg-neutral-900  p-3 my-1 rounded-lg">
             <div className="flex justify-between items-center">
                 {/* Name */}
                 <p className='text-[15px]'>{props.name}</p>
@@ -12,7 +12,7 @@ function Chat({props}) {
 
             {/* Description */}
             <p className='text-[12px] mt-1'>{props.description}</p>
-        </div>
+        </button>
     )
 }
 
