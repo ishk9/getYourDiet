854c3d6db2a903ff22198346315207565f936f30
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-04-24 14:18:53 +0530

    Feature: Added History component!

---
+++ client/app/Home/page.js
@@ -0,0 +1,10 @@
+import History from '@/components/History'
+import React from 'react'
+
+export default function Home() {
+    return (
+        <main className='flex flex-row h-screen w-screen'>
+            <History />
+        </main>
+    )
+}
