e60b03cc6c6729a1564f3b05b5d749715df7bcf1
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-04-28 17:00:47 +0530

    Added the header section!

--- client/app/Home/page.js
+++ client/app/Home/page.js
@@ -2,13 +2,29 @@
 import React, { useState } from 'react'
 import History from '@/components/History'
 import { IoReorderThreeOutline } from "react-icons/io5";
-
+import { BsThreeDots } from "react-icons/bs";
 
 export default function Home() {
     const [history, showHistory] = useState(false);
+    const [diets, numberDiets] = useState(10);
     return (
         <main className='flex flex-row h-screen w-screen'>
-            <div className={`${history ? "w-4/5" : "w-0"} lg:w-1/5 overflow-auto`}>
+            <div className={`${history ? "w-3/5" : "w-0"} lg:w-1/5 overflow-auto`}>
+
+                <div className='flex justify-between px-5 items-center mt-4'>
+                    <div className='flex'>
+                        <h1 className='text-lg font-semibold mr-2'>Diets</h1>
+                        <div className='flex justify-center items-center h-8 w-8 rounded-full bg-slate-600'>
+                            <p className='text-[13px]'>{diets}</p>
+                        </div>
+                    </div>
+                    <button onClick={() => {}}>
+                        <BsThreeDots />
+                    </button>
+
+                </div>
+
+
                 <History />
             </div>
 
