fbdedf19e3eab403f57ed7ed9f0e9a5fdd34e4df
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-04-28 14:53:00 +0530

    Fix:Added button for displaying history!

--- client/app/Home/page.js
+++ client/app/Home/page.js
@@ -1,10 +1,28 @@
+"use client";
+import React, { useState } from 'react'
 import History from '@/components/History'
-import React from 'react'
+import { IoReorderThreeOutline } from "react-icons/io5";
+
 
 export default function Home() {
+    const [history, showHistory] = useState(false);
     return (
         <main className='flex flex-row h-screen w-screen'>
-            <History />
+            <div className={`${history ? "w-3/5" : "w-0"} lg:w-1/5 overflow-auto`}>
+                <History />
+            </div>
+
+            <div className="block sm:hidden">
+                <button 
+                    onClick = {() => {
+                        console.log('Clicked');
+                        showHistory(!history)
+                    }}
+                    className='mt-2 ml-2 flex justify-center items-start'>
+                    <IoReorderThreeOutline size={26} />
+                </button>
+            </div>
+
         </main>
     )
 }
