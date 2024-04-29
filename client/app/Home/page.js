94b01035fdcb736b2880ac24f753c44defccbfe5
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-04-29 17:06:37 +0530

    Fix: Added backdrop

--- client/app/Home/page.js
+++ client/app/Home/page.js
@@ -11,7 +11,7 @@ export default function Home() {
         <main className='flex flex-row h-screen w-screen'>
             <div className={`${history ? "w-3/5" : "w-0"} lg:w-1/5 overflow-auto`}>
 
-                <div className='flex justify-between px-5 items-center mt-4'>
+                <div className='flex justify-between px-5 items-center mt-4 sticky top-0 backdrop-filter backdrop-brightness-0 backdrop-opacity-100'>
                     <div className='flex'>
                         <h1 className='text-lg font-semibold mr-2'>Diets</h1>
                         <div className='flex justify-center items-center h-8 w-8 rounded-full bg-slate-600'>
