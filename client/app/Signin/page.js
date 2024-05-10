1b1db58ce876cbd1b63f5e0bb8b95cd12e7bd239
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-05-10 18:24:13 +0530

    Style: Fixed the flex

--- client/app/Signin/page.js
+++ client/app/Signin/page.js
@@ -6,30 +6,27 @@ import useStore from '../store';
 function SigninPage() {
     const {setOpenSignInModal} = useStore();
     return (
-        <div className="h-4/6 w-1/5 p-4 rounded-xl absolute z-10 flex flex-col justify-evenly items-center bg-slate-100 border-2 shadow-md">
+        <div className="h-full w-full p-4 rounded-xl absolute z-10 flex flex-col justify-evenly items-center bg-slate-100">
             <h1 className='text-black text-[24px] font-semibold'>Signin</h1>
             
             <div className='flex w-full justify-center items-center'>
-                <div className='flex flex-col'>
-                    <p className='text-black font-semibold text-md'>Country</p>
-                    <input className='my-1 w-1/4 p-2 rounded-md text-black mr-2' placeholder='+91' />
-                </div>
-
-                <div className='flex flex-col'>
-                    <p className='text-black font-semibold text-md'>Phone Number</p>
-                    <input className='my-1 w-2/4 p-2 rounded-md text-black' placeholder='985762XXXX' />
-                </div>
-
+                <input className='my-1 w-1/4 p-2 rounded-md text-black mr-2' placeholder='+91' />
+                <input className='my-1 w-3/4 p-2 rounded-md text-black' placeholder='985762XXXX' />
             </div>
-               
-            <button className='w-3/4 rounded-md py-3 mt-2 bg-slate-700'>
-                <p>Submit</p>
-            </button>
-            <button 
-                onClick={setOpenSignInModal}
-                className='w-3/4 rounded-md py-3 mt-2 bg-slate-700'>
-                <p>Sign Up</p>
-            </button>
+            
+
+            <div className='flex flex-col w-3/4 justify-center items-center'>
+                <button className='w-full rounded-md py-3 mt-2 bg-slate-700 hover:bg-slate-600'>
+                    <p>Submit</p>
+                </button>
+
+                <button className='mt-2'onClick={setOpenSignInModal}>
+                    <p className='text-black text-[12px]'>
+                        Dont have an account? <span className='font-semibold text-[15px]'>Sign up</span>
+                    </p>
+                </button>
+            </div>
+
         </div>
     )
 }
