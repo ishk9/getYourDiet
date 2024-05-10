628479d59893bc1852fd545908c63de2c25a2d4a
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-05-10 18:37:43 +0530

    Fix: Added responsiveness!

--- client/app/Onboarding/page.js
+++ client/app/Onboarding/page.js
@@ -8,7 +8,7 @@ import SignupPage from '../Signup/page';
 function OnboardingPage() {
     const {openSignInModal} = useStore();
     return (
-        <div className="h-4/6 w-1/5 p-4 rounded-xl absolute z-10 flex flex-col justify-evenly items-center bg-slate-100 border-2 shadow-md">
+        <div className="h-4/6 md:w-1/5 w-3/5 p-4 rounded-xl absolute z-10 flex flex-col justify-evenly items-center bg-slate-100 border-2 shadow-md">
             {
                 openSignInModal ? <SigninPage /> : <SignupPage />
             }
