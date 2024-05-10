6a424042be62fa8b0c48b379a6427ad418338f0b
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-05-10 15:28:58 +0530

    Feature: Added modal for signin

---
+++ client/app/Onboarding/page.js
@@ -0,0 +1,19 @@
+"use client";
+import React from 'react'
+import useStore from '../store';
+import SigninPage from '../Signin/page';
+import SignupPage from '../Signup/page';
+
+
+function OnboardingPage() {
+    const {openSignInModal} = useStore();
+    return (
+        <div className="h-4/6 w-1/5 p-4 rounded-xl absolute z-10 flex flex-col justify-evenly items-center bg-slate-100 border-2 shadow-md">
+            {
+                openSignInModal ? <SigninPage /> : <SignupPage />
+            }
+        </div>
+    )
+}
+
+export default OnboardingPage
