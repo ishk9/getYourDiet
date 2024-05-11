ea9d2f4e79bcb8c378e734d83c524b7bb2f0e492
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-05-11 19:35:18 +0530

    Replaced links with buttons!

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -4,23 +4,24 @@ import React from 'react'
 import useStore from '@/app/store'
 
 function Navbar() {
+    
     const {openOnboardingModal, setOpenOnboardingModal} = useStore();
     return (
-        <navbar className="flex flex-row w-full justify-between items-center px-2">
+        <navbar className="flex flex-row w-full justify-between items-center px-10 py-2">
             <Link href="/">
-                <p className="text-black text-lg font-bold">GetYourDiet</p>
+                <p className="text-black text-lg font-bold uppercase">GetYourDiet</p>
             </Link>
 
             <div className="flex flex-row w-2/4 lg:w-1/4 justify-evenly items-center">
-                <Link className="hover:-translate-y-0.5" href="/">
-                    <p className="text-black font-semibold">Our Method</p>
-                </Link>
-                <Link className="hover:-translate-y-0.5" href="/Pricing">
-                    <p className="text-black font-semibold">Pricing</p>
-                </Link>
-                <Link className="hover:-translate-y-0.5" href="/Reviews">
-                    <p className="text-black font-semibold hidden sm:block">Reviews</p>
-                </Link>
+                <button className="hover:-translate-y-0.5" href="/">
+                    <p className="text-black text-sm font-bold uppercase">Our Method</p>
+                </button>
+                <button className="hover:-translate-y-0.5" href="/Pricing">
+                    <p className="text-black text-sm font-bold uppercase">Pricing</p>
+                </button>
+                <button  className="hover:-translate-y-0.5" href="/Reviews">
+                    <p className="text-black text-sm font-bold hidden sm:block uppercase">Reviews</p>
+                </button>
             </div>
 
             <div>
