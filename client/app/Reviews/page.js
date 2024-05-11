d139d563514e3aea00ee6abbb900db85e7c0ae5f
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-05-11 19:34:14 +0530

    Fix: Adjusted layout

--- client/app/Reviews/page.js
+++ client/app/Reviews/page.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { IoIosArrowUp } from "react-icons/io";
 
 export default function Reviews() {
+    const scrollToTop = () => {
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+    })
+    };
     return (
-        <div>
+        <div className="h-screen w-screen bg-red-500 px-10 py-2">
             <p>Reviews</p>
+            <button onClick={scrollToTop}>
+                <IoIosArrowUp  color="black" size={40}/>
+            </button>
+
         </div>
     )
 }
