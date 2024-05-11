374cc41417f64b0ee78f15f90c76b5e53a0b37b7
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-05-11 22:32:26 +0530

    Feature: Removed the edit button!

--- client/app/Landing/page.js
+++ client/app/Landing/page.js
@@ -9,7 +9,7 @@ import OnboardingPage from '../Onboarding/page';
 export default function LandingPage() {
     const {openOnboardingModal} = useStore();
     return (
-        <div className="flex w-screen h-screen justify-center items-center px-10 py-2">
+        <div className="flex w-screen h-screen justify-center items-center px-10 py-2 bg-[#DE3B3C]">
             {
                 openOnboardingModal && <OnboardingPage />
             }
