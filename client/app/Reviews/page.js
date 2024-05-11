2a1a94153de9649b491711e8a05aa4598d32d225
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-05-11 22:31:28 +0530

    Feature: Added arrow to move to top!

--- client/app/Reviews/page.js
+++ client/app/Reviews/page.js
@@ -9,9 +9,14 @@ export default function Reviews() {
     })
     };
     return (
-        <div className="h-screen w-screen bg-red-500 px-10 py-2">
-            <p>Reviews</p>
-            <button onClick={scrollToTop}>
+        <div className="h-screen w-screen flex flex-col justify-center items-center bg-red-500 px-10 py-2">
+            <h1 className="">Reviews</h1>
+
+            <p>Share your thoughts with us</p>
+            <button 
+                onClick={scrollToTop}
+                className="transition-transform duration-300 ease-in-out transform hover:-translate-y-1"
+            >
                 <IoIosArrowUp  color="black" size={40}/>
             </button>
 
