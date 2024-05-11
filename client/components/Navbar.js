361eb534c1338aaef04c49a0ebcb4d051af603e6
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-05-11 22:13:30 +0530

    Style: Switched to red

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,25 +1,24 @@
 "use client";
-import Link from 'next/link'
 import React from 'react'
 import useStore from '@/app/store'
 
 function Navbar() {
-    
     const {openOnboardingModal, setOpenOnboardingModal} = useStore();
     return (
-        <navbar className="flex flex-row w-full justify-between items-center px-10 py-2">
-            <Link href="/">
+        <navbar className="flex flex-row w-full justify-between items-center px-10 py-2 bg-[#DE3B3C]">
+            <button>
                 <p className="text-black text-lg font-bold uppercase">GetYourDiet</p>
-            </Link>
+            </button>
 
             <div className="flex flex-row w-2/4 lg:w-1/4 justify-evenly items-center">
-                <button className="hover:-translate-y-0.5" href="/">
-                    <p className="text-black text-sm font-bold uppercase">Our Method</p>
+                <button className="hover:-translate-y-0.5">
+                    <p className="text-black text-sm font-bold">Our Method</p>
+    
                 </button>
-                <button className="hover:-translate-y-0.5" href="/Pricing">
+                <button className="hover:-translate-y-0.5">
                     <p className="text-black text-sm font-bold uppercase">Pricing</p>
                 </button>
-                <button  className="hover:-translate-y-0.5" href="/Reviews">
+                <button  className="hover:-translate-y-0.5">
                     <p className="text-black text-sm font-bold hidden sm:block uppercase">Reviews</p>
                 </button>
             </div>
