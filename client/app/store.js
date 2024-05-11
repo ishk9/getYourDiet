a2143ad8197d2b40596e54c0613e2f6d481fae4e
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-05-11 22:30:17 +0530

    Shadcn config!

--- client/app/store.js
+++ client/app/store.js
@@ -3,10 +3,8 @@ import { create } from 'zustand';
 const useStore = create((set) => ({
   openOnboardingModal: false,
   openSignInModal: true,
-  openCreateDiet: false,
   setOpenOnboardingModal: () => set((state) => ({ openOnboardingModal: !state.openOnboardingModal})),
   setOpenSignInModal: () => set((state) => ({ openSignInModal: !state.openSignInModal})),
-  setOpenCreateDiet: () => set((state) => ({ openCreateDiet: !state.openCreateDiet})),
 }));
 
 export default useStore;
