d5fa8d3b9d9402ae43331a28d26cc0b62cc69c6c
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-05-12 20:27:38 +0530

    Fix: Reduced color opacity

--- client/components/Chat.js
+++ client/components/Chat.js
@@ -7,22 +7,22 @@ function Chat({props}) {
         <div className="flex flex-col justify-center items-start hover:bg-neutral-900  p-3 my-1 rounded-lg">
             <div className="flex w-full justify-between items-center">
                 {/* Name */}
-                <p className='text-[15px]'>{props.name}</p>
+                <p className='text-[15px] font-medium text-white'>{props.name}</p>
                 {/* Date */}
-                <p className='text-[12px]'>{props.date}</p>
+                <p className='text-[12px] font-medium text-white'>{props.date}</p>
             </div>
 
             <div className='flex w-full justify-between items-center'>
                 {/* Description */}
-                <p className='text-[12px] mt-1'>{props.description}</p>
+                <p className='text-[12px] mt-1 text-white'>{props.description}</p>
 
                 <div className='flex'>
                     <button onClick={() => {}}>
-                        <MdOutlineDelete className='hover:text-white text-zinc-800' size={18} />
+                        <MdOutlineDelete className='hover:text-white text-zinc-600' size={18} />
                     </button>
                         
                     <button onClick={() => {}}>
-                        <MdOutlineEdit className='hover:text-white text-zinc-800' size={18} />
+                        <MdOutlineEdit className='hover:text-white text-zinc-600' size={18} />
                     </button>
 
                 </div>
