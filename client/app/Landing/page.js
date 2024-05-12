d41fc68ee66d8dbae1466a7c63c08131875cbdd3
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-05-12 20:22:46 +0530

    Style: Changed color to yellow!

--- client/app/Landing/page.js
+++ client/app/Landing/page.js
@@ -9,7 +9,7 @@ import OnboardingPage from '../Onboarding/page';
 export default function LandingPage() {
     const {openOnboardingModal} = useStore();
     return (
-        <div className="flex w-screen h-screen justify-center items-center px-10 py-2">
+        <div className="flex w-screen h-screen justify-center items-center px-10 py-2 bg-[#E7C25A]">
             {
                 openOnboardingModal && <OnboardingPage />
             }
