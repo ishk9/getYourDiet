176c13f5001339dce88583622aaa191b8de773af
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-05-12 20:21:29 +0530

    Added input fields!

--- client/app/Reviews/page.js
+++ client/app/Reviews/page.js
@@ -10,9 +10,39 @@ export default function Reviews() {
     })
     };
     return (
-        <div className="h-screen w-screen bg-red-500 px-10 py-2">
-            <p>Reviews</p>
-            <button onClick={scrollToTop}>
+        <div className="h-screen w-screen flex flex-col justify-evenly items-center px-10 py-2 bg-red-500">
+            <div className="flex flex-col">
+                <h1 className="text-5xl font-semibold">Would love to hear from you.</h1>
+                <p className="text-5xl font-semibold">Get in touch!</p>
+            </div>
+
+            <div className="flex flex-col justify-center items-center mt-4">
+                <div className="flex w-screen justify-center items-center">
+                    <textarea
+                        id="textarea"
+                        // name="textarea"
+                        className="border border-gray-300 p-2 w-1/2 h-32 resize-none outline-none rounded-md font-mono"
+                        // value={textareaValue}
+                        // onChange={(e) => setTextareaValue(e.target.value)}
+                    />
+
+                </div>
+                <div className='flex mt-3'>
+                    <input 
+                        type='checkbox'
+                    />
+                    <p className='font-mono ml-1'>Send regular updates</p>
+                </div>
+            </div>
+
+            <div className='w-screen flex justify-center items-center'>
+                <button className='px-2 py-4 border-2 w-1/4 border-black rounded-md hover:scale-105'>
+                    <p className='font-serif'>Submit</p>
+                </button>
+            </div>
+
+
+            <button className='hover:-translate-y-1'onClick={scrollToTop}>
                 <IoIosArrowUp  color="black" size={40}/>
             </button>
 
