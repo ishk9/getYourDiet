f51ffb66718f9b1a5f067d48311094b4644c08ba
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-05-14 09:50:04 +0530

    Style: Added color gradient!

--- client/app/Home/page.js
+++ client/app/Home/page.js
@@ -13,10 +13,10 @@ export default function Home() {
     return (
         <main className='flex flex-row h-screen w-screen bg-[#E7C25A]'>
             {/* History Section */}
-            <div className={`${historyOpen ? "w-3/5 md:w-1/5" : "w-0"} overflow-auto bg-black transition-width duration-300`}>
-                <div className='flex justify-between px-5 items-center mt-4 sticky top-0 backdrop-filter backdrop-brightness-0 backdrop-opacity-100'>
+            <div className={`${historyOpen ? "w-3/5 md:w-1/5" : "w-0"} overflow-auto  bg-black bg-gradient-to-tr from-zinc-900/50 to-zinc-700/30 transition-width duration-300`}>
+                <div className='flex justify-between px-5 items-center mt-4 sticky top-0 backdrop-filter backdrop-brightness-0 backdrop-opacity-100  bg-black bg-gradient-to-tr from-zinc-900/50 to-zinc-700/30'>
                     <div className='flex'>
-                        <h1 className='text-lg font-semibold mr-2'>Diets</h1>
+                        <h1 className='text-lg font-semibold mr-2 text-white'>Diets</h1>
                         <div className='flex justify-center items-center h-8 w-8 rounded-full bg-slate-600'>
                             <p className='text-[13px]'>10</p>
                         </div>
