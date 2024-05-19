772f5af962c3293fc3513e591f0cbf4b05099d4f
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-05-20 00:13:04 +0530

    Added dummy data

--- client/app/store.js
+++ client/app/store.js
@@ -1,10 +1,16 @@
 import { create } from 'zustand';
 
 const useStore = create((set) => ({
+  dummyData: [
+    {id: 1, name: "Ishaan", date: '24 April', description: 'abcsefwdcf'},
+    {id: 2, name: "Khullar", date: '24 April', description: 'abcscwdvnovnefwdcf'},
+  ],
   openOnboardingModal: false,
   openSignInModal: true,
   setOpenOnboardingModal: () => set((state) => ({ openOnboardingModal: !state.openOnboardingModal})),
   setOpenSignInModal: () => set((state) => ({ openSignInModal: !state.openSignInModal})),
+  addToDummyData: (newData) =>
+    set((state) => ({ dummyData: [...state.dummyData, newData] })),
 }));
 
 export default useStore;
