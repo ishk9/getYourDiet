836073de39da0e8a6ffabf4aa9c1c53e94c901b5
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-05-22 22:38:56 +0530

    Added input!

--- client/components/UserInputSection.js
+++ client/components/UserInputSection.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 function UserInputSection() {
     return (
-        <div>
-        
+        <div className='h-full w-full flex flex-col'>
+            <input />
+            <input />
+            <input />
+            <input />
+            <input />
+            <input />
+            <input />
         </div>
     )
 }
