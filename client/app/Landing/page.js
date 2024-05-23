598a97d9fc0beb5a6a882ff36b696fff1845a3be
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-05-23 08:28:32 +0530

    Feature: Added animation to the heading

--- client/app/Landing/page.js
+++ client/app/Landing/page.js
@@ -4,10 +4,11 @@ import { FaArrowRightLong } from "react-icons/fa6";
 import Link from 'next/link';
 import useStore from '../store';
 import OnboardingPage from '../Onboarding/page';
-
+import { FlipWords } from '@/components/ui/flip-words';
 
 export default function LandingPage() {
     const {openOnboardingModal} = useStore();
+    const words = ["tasty", "healthy", "nutritious"];
     return (
         <div className="flex w-screen h-screen justify-center items-center px-10 py-2 bg-[#E7C25A]">
             {
@@ -16,7 +17,9 @@ export default function LandingPage() {
 
             <div className="flex lg:w-3/4 w-11/12 cursor-default">
                 <div className="flex flex-col w-full justify-center items-start">
-                    <h1 className="font-extrabold text-black lg:text-[9em] text-[3em] leading-[1] tracking-[-2px]">YOU CAN EAT ANYTHING</h1>
+                    <h1 className="font-extrabold text-black lg:text-[7em] text-[3em] leading-[1] tracking-[-2px] uppercase">
+                        Eat food that is <FlipWords words={words}/>
+                    </h1>
 
                     <p className="mt-6 lg:w-1/2 w-full flex lg:text-base font-semibold text-[11px] text-black">
                         Our app is designed to help you successfully create a diet based on your body and the food you like. Easily optimize your eating schedule and receive community support
