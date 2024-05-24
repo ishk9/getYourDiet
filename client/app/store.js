35d84f93d13ea2fb971cf3c45e5c121fb894e4db
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-05-24 20:20:38 +0530

    Added function to perform ops

--- client/app/store.js
+++ client/app/store.js
@@ -1,16 +1,24 @@
 import { create } from 'zustand';
 
 const useStore = create((set) => ({
-  dummyData: [
-    {id: 1, name: "Ishaan", date: '24 April', description: 'abcsefwdcf'},
-    {id: 2, name: "Khullar", date: '24 April', description: 'abcscwdvnovnefwdcf'},
-  ],
   openOnboardingModal: false,
   openSignInModal: true,
   setOpenOnboardingModal: () => set((state) => ({ openOnboardingModal: !state.openOnboardingModal})),
   setOpenSignInModal: () => set((state) => ({ openSignInModal: !state.openSignInModal})),
-  addToDummyData: (newData) =>
-    set((state) => ({ dummyData: [...state.dummyData, newData] })),
+  
+  list: [
+    {name: "Ishaan", date: '24 April', description: 'abcsefwdcf'},
+    {name: "Khullar", date: '24 April', description: 'abcscwdvnovnefwdcf'},
+    {name: "xyz", date: '24 April', description: 'abcscwdvnovnefwdcf'},
+  ],
+  addItem: (item) => set((state) => ({ list: [...state.list, item] })),
+  updateItem: (index, newItem) => set((state) => ({
+    list: state.list.map((item, i) => (i === index ? newItem : item)),
+  })),
+  removeItem: (index) => set((state) => ({
+    list: state.list.filter((_, i) => i !== index),
+  })),
+
 }));
 
 export default useStore;
