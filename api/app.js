457e2e178ac15b77a18e22d009cc3a81a91dccab
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-05-25 21:34:25 +0530

    Imported routes!

--- api/app.js
+++ api/app.js
@@ -3,21 +3,22 @@ import cors from 'cors';
 import router from "./routes/user.js";
 import dataRouter from './routes/data.js';
 import connectMongoDb from './connection.js';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
 
 // Routes and other configurations
 app.use("/user", router);
 app.use("/data", dataRouter);
 
-connectMongoDb(process.env.MONGOURL).then(() => {
-  console.log("Connected to MongoDB");
-}).catch((err) => {
-  console.log(err);
-});
+// connectMongoDb(process.env.MONGOURL).then(() => {
+//   console.log("Connected to MongoDB");
+// }).catch((err) => {
+//   console.log(err);
+// });
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
