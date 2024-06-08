23b20a7f5aa975be022303cc23724f27d690bdde
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-06-08 19:02:46 +0530

    Removed auth for now

--- client/app/page.js
+++ client/app/page.js
@@ -3,24 +3,14 @@ import Navbar from "@/components/Navbar";
 import LandingPage from "./Landing/page";
 import PricingPage from "./Pricing/page";
 import Reviews from "./Reviews/page";
-import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Home() {
-  const { user } = useAuth0();
   return (
     <main className="overflow-x-hidden">
-      {/* {
-        user ?
-          <Home />
-        :
-        <> */}
-          <Navbar />
-          <LandingPage />
-          <PricingPage/>
-          <Reviews />
-        {/* </>
-       
-      } */}
+      <Navbar />
+      <LandingPage />
+      <PricingPage/>
+      <Reviews />
     </main>
   );
 }
