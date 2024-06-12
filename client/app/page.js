7aaed4a495441062b804581fb8e4e2a070d6e550
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-06-12 12:43:42 +0530

    Added landing page!

--- client/app/page.js
+++ client/app/page.js
@@ -1,16 +1,11 @@
 "use client";
-import Navbar from "@/components/Navbar";
 import LandingPage from "./Landing/page";
-import PricingPage from "./Pricing/page";
-import Reviews from "./Reviews/page";
 
 export default function Home() {
+
   return (
     <main className="overflow-x-hidden">
-      <Navbar />
       <LandingPage />
-      <PricingPage/>
-      <Reviews />
     </main>
   );
 }
