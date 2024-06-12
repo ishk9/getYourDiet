793a60c525d2e3c7bcffb9d0a12812d6a5afe1af
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-06-12 12:43:12 +0530

    Fix: Changed bg icon

--- client/app/Home/page.js
+++ client/app/Home/page.js
@@ -18,17 +18,15 @@ export default function Home() {
     return (
         <main className='flex flex-row h-screen w-screen bg-[#E7C25A]'>
             {/* History Section */}
-            <div className={`${historyOpen ? "w-3/5 md:w-4/12" : "w-0"} overflow-auto  bg-black bg-gradient-to-tr from-zinc-900/50 to-zinc-700/30 transition-width duration-300`}>
+            <div className={`${historyOpen ? "w-3/12" : "w-0"} overflow-auto  bg-black bg-gradient-to-tr from-zinc-900/50 to-zinc-700/30 transition-width duration-300`}>
                 <div className='flex justify-between px-5 items-center mt-4 sticky top-0 backdrop-filter backdrop-brightness-0 backdrop-opacity-100  bg-black bg-gradient-to-tr from-zinc-900/50 to-zinc-700/30'>
-                    <div className='flex'>
+                    <div className='flex justify-center items-center'>
                         <h1 className='text-lg font-semibold mr-2 text-white'>Diets</h1>
-                        <div className='flex justify-center items-center h-8 w-8 rounded-full bg-slate-600'>
-                            <p className='text-[13px]'>10</p>
+                        <div className='flex justify-center items-center h-8 w-8 rounded-full bg-[#CBBCA5]'>
+                            <p className='text-[13px] font-semibold'>10</p>
                         </div>
                     </div>
-                    <button onClick={toggleHistory}>
-                        
-                    </button>
+                    
                 </div>
                 <History />
 
@@ -50,12 +48,12 @@ export default function Home() {
             </div>
 
             {/* Diet Formation Section */}
-            <div className={`${historyOpen ? "md:w-3/5" : "w-full"} flex flex-col justify-center items-center`}>
+            <div className={`${historyOpen ? "w-6/12" : "w-full"} flex flex-col justify-center items-center`}>
                 <h1 className='text-black'>Welcome Onboard!</h1>
                 <p>{list[activeChat].about}</p>
             </div>
             
-            <div className={`md:w-3/6 flex flex-col justify-evenly items-center bg-black bg-gradient-to-tr from-zinc-900/50 to-zinc-700/30 transition-width w-3/5 py-2 p-4`}>
+            <div className={`w-3/12 flex flex-col justify-evenly items-center bg-black bg-gradient-to-tr from-zinc-900/50 to-zinc-700/30 transition-width py-2 p-4`}>
                 <h1 className='text-white font-bold text-[1.4em]'>Customize your diet</h1>
                 <UserInputSection />
             </div>
