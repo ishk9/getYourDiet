a3f4e437732d754b3acf1b09874ea4c22bb9b480
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-06-13 18:11:14 +0530

    Fix

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,11 +1,8 @@
 "use client";
 import Link from 'next/link'
 import { FaXTwitter } from "react-icons/fa6";
-import { useAuth0 } from "@auth0/auth0-react";
 
 function Navbar() {
-    const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
-    console.log(user);
     const scrollToSection = (sectionId) => {
         document.getElementById(sectionId).scrollIntoView({
             behavior: 'smooth'
@@ -14,9 +11,9 @@ function Navbar() {
     return (
         <nav className="flex flex-row w-full justify-between items-center px-16 py-2 bg-transparent fixed  top-0 z-50">
             <div className="flex w-1/2 items-center">
-                <button>
+                <Link href="/">
                     <p className="text-black text-2xl font-extrabold uppercase">GetYourDiet</p>
-                </button>
+                </Link>
 
                 <div className="flex flex-row w-1/4 justify-evenly items-center ml-16">
                     <button 
@@ -40,20 +37,12 @@ function Navbar() {
                     />
                 </Link>
 
-                {
-                    isAuthenticated ? 
-                        <button 
-                            onClick={() => logout()}
-                            className='flex ml-4 justify-center items-center border-2 border-black p-2 px-3 rounded-lg hover:-translate-y-0.5'>
-                            <p className="text-black text-sm font-semibold">Logout</p>
-                        </button>
-                        :
-                        <button 
-                            onClick={() => loginWithRedirect()}
-                            className='flex ml-4 justify-center items-center border-2 border-black p-2 px-3 rounded-lg hover:-translate-y-0.5'>
-                            <p className="text-black text-sm font-semibold">Login</p>
-                        </button>
-                }
+                <button 
+                    onClick={() => {}}
+                    className='flex ml-4 justify-center items-center border-2 border-black p-2 px-3 rounded-lg hover:-translate-y-0.5'>
+                    <p className="text-black text-sm font-semibold">Login</p>
+                </button>
+                
             </div>
         </nav>
     )
