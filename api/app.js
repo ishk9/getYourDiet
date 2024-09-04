ab29cf9799ea754f8f6b8435d8a73c2eccfca4d5
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-09-04 18:11:25 +0530

    added swagger

--- api/app.js
+++ api/app.js
@@ -2,17 +2,16 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-// import swaggerUi from 'swagger-ui-express';
-// import swaggerDocument from './swagger.json';
+import swaggerDocs from './swagger.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
+    swaggerDocs(app, PORT);
     console.log(`Server is running on port ${PORT}`);
 });
 
---
+++ api/swagger.js
@@ -0,0 +1,29 @@
+import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Swagger',
+      description: 'For getMyDiet app',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./routes/*.js'],
+}
+
+const swaggerSpec = swaggerJsdoc(options)
+
+function swaggerDocs(app, port) {
+  // Swagger Page
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
+  // Documentation in JSON format
+  app.get('/docs.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    res.send(swaggerSpec)
+  })
+}
+
+export default swaggerDocs
