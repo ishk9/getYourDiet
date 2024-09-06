1f0d560dc500b515934cb207b8064e7174778436
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-09-06 18:31:00 +0530

    added jwt token

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,9 @@
 import User from '../models/user.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+
+
 
 const signupUser = async (req, res) => {
     const { name, email, phoneNumber, password } = req.body;
@@ -8,7 +12,10 @@ const signupUser = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, 10);
         const user = new User({ name, email, phoneNumber, password: hashedPassword });
         await user.save();
-        res.status(201).json({ message: 'User signed up successfully', data: user });
+        console.log("User: ", user);
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+        console.log("Token: ", token);
+        res.status(201).json({ message: 'User signed up successfully', data: user, token });
     } catch (err) {
         res.status(500).json({ error: 'Internal server error' });
     }
@@ -26,7 +33,9 @@ const loginUser = async(req, res) => {
         if (!isMatch) {
             return res.status(400).json({ error: 'Invalid email or password' });
         }
-        res.status(200).json({ message: 'Login successful', data: user });
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+        console.log("Token: ", token);
+        res.status(200).json({ message: 'Login successful', data: user, token });
     } catch(err){
         res.status(500).json({ error: 'Internal server error' });
     }
