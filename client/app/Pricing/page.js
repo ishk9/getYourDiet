81c50739253dade3856c6f5459ba954b72723d20
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-09-14 16:46:33 +0530

    added responsiveness

--- client/app/Pricing/page.js
+++ client/app/Pricing/page.js
@@ -1,38 +1,30 @@
 "use client";
-import PriceCard from '@/components/PriceCard'
-import React, { useEffect } from 'react'
+import PriceCard from '@/components/PriceCard';
 
 const prices = [
     {id: 1, title: 'Single', price: 'free', description: 'Best for single users', popular: 0, points: [
         { id: 1, point: 'Single diet plan' },
-        { id: 2, point: 'Custom diet generation' },
-        { id: 3, point: 'Shareable diet plans' },
+        { id: 2, point: 'Shareable diet plans' },
     ]},
     {id: 2, title: 'Family', price: '15', description: 'Best for family', popular: 1, points: [
         { id: 1, point: 'Multiple diets for family' },
-        { id: 2, point: 'Custom diet generation' },
-        // { id: 3, point: 'Task tracking' },
-        { id: 4, point: 'Shareable diet plans' },
-]},
+        { id: 2, point: 'Shareable diet plans' },
+    ]},
     {id: 3, title: 'Professional', price: '30', description: 'Best for Professional', popular: 0, points: [
         { id: 1, point: 'Unlimited diet plans' },
-        { id: 2, point: 'Custom diet generation' },
-        // { id: 3, point: 'Advanced tracking features' },
-        { id: 4, point: 'Shareable diet plans' },
+        { id: 2, point: 'Shareable diet plans' },
     ]},
 ]
 
 export default function PricingPage() {
-
     return (
-        <div id="pricing" className="flex flex-col w-screen h-screen items-center px-10 py-2 overflow-hidden bg-[#6EC0FF]">        
-            <h1 className="text-[3em] font-semibold mt-20 text-black">Pricing</h1>
+        <div id="pricing" className="flex flex-col w-screen h-auto md:h-screen items-center px-5 py-10 md:py-2 bg-[#6EC0FF]">        
+            <h1 className="text-[2em] md:text-[3em] font-semibold mt-10 md:mt-20 text-black">Pricing</h1>
             <div className="w-full md:w-[44%] flex flex-col justify-center items-center">
                 <p className='mt-3 text-black font-semibold'>Choose the plan that&apos;s right for you</p>
             </div>
 
-            
-            <div className='flex flex-col md:flex-row h-full w-full justify-center items-center'>
+            <div className='flex flex-col md:flex-row h-full w-full justify-center items-center mt-8 md:mt-12'>
                 {
                     prices.map((item, id) => (
                         <PriceCard key={id} props={item}/>
@@ -42,4 +34,3 @@ export default function PricingPage() {
         </div>
     )
 }
-
--- client/components/PriceCard.js
+++ client/components/PriceCard.js
@@ -1,38 +1,35 @@
 import { FaCircleCheck } from "react-icons/fa6";
-function PriceCard({props}) {
+
+function PriceCard({ props }) {
     return (
-        <button className={`flex flex-col ${props.popular ? "h-[30%] md:h-5/6" : "h-[30%] md:h-4/6"} md:w-1/5 w-full sm:w-full shadow-lg shadow-slate-600 bg-slate-50 rounded-xl mx-3 p-5 transition-transform duration-300 hover:scale-105`}>
-            <div className="h-3/4 flex flex-col justify-center items-start">
-                <h1 className="text-base text-black font-semibold uppercase">{props.title}</h1>
+        <button className={`flex flex-col ${props.popular ? "h-[40%] md:h-5/6" : "h-[40%] md:h-4/6"} w-full md:w-[25%] mb-8 md:mb-0 shadow-lg shadow-slate-600 bg-slate-50 rounded-xl mx-3 p-5 transition-transform duration-300 hover:scale-105`}>
+            <div className="h-full flex flex-col justify-center items-start">
+                <h1 className="text-base md:text-lg text-black font-semibold uppercase">{props.title}</h1>
                 {
-                    props.price === "free" ?
-                    <p className="text-black font-bold md:text-[24px] uppercase text-[11px]">{props.price}</p>
+                    props.price === "free" ? 
+                    <p className="text-black font-bold md:text-[24px] uppercase text-[14px] mt-1">{props.price}</p>
                     :
-                    <p className="text-black font-bold md:text-[28px] text-[11px]">${props.price}</p>
+                    <p className="text-black font-bold md:text-[28px] text-[14px] mt-1">${props.price}</p>
                 }
-                {/* <p className='text-black '>{props.description}</p> */}
-                <p className='text-gray-500 font-medium text-sm mt-2'>What is included?</p>
-                <div className="flex flex-col justify-center items-start mt-1">
+                <p className='text-gray-500 font-medium text-xs md:text-sm mt-2'>What is included?</p>
+                <div className="flex flex-col justify-center items-start mt-2">
                     {
                         props.points.map((point) => (
-                            
-                            <div key={point.id} className="flex justify-center items-center mt-2">
-                                <FaCircleCheck />
-                                <p className="ml-1">{point.point}</p>
+                            <div key={point.id} className="flex justify-start items-center mt-2">
+                                <FaCircleCheck className="text-green-500" />
+                                <p className="ml-2 text-sm md:text-base">{point.point}</p>
                             </div>
-
                         ))
                     }
                 </div>
             </div>
 
-
             <div
-                className={`border-2 mt-8 w-full ${props.popular ? "bg-black": "bg-white"} rounded-lg`}>
-                <p className={` ${props.popular ? "text-white": ""} h-full w-full py-2 px-4 font-semibold md:text-base text-[12px]`}>Get Started</p>
+                className={`border-2 mt-4 md:mt-8 w-full ${props.popular ? "bg-black" : "bg-white"} rounded-lg`}>
+                <p className={`py-2 px-4 font-semibold ${props.popular ? "text-white" : "text-black"} md:text-base text-[12px]`}>Get Started</p>
             </div>
         </button>
-    )
+    );
 }
 
-export default PriceCard
+export default PriceCard;
