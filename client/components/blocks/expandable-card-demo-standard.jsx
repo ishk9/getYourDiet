07bb457dd465f810a5500d3eaeb45a041edede9b
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-09-21 21:29:33 +0530

    fixed flow for pricing and diet page

--- client/components/blocks/expandable-card-demo-standard.jsx
+++ client/components/blocks/expandable-card-demo-standard.jsx
@@ -1,12 +1,18 @@
 "use client";
 import Image from "next/image";
-import React, { useEffect, useId, useRef, useState } from "react";
+import  { useEffect, useId, useRef, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { useOutsideClick } from "../hooks/use-outside-click.js";
 import { getDiet } from "@/lib/services.js";
+import { MdLockOutline } from "react-icons/md";
+import { useRouter } from "next/navigation";
 
 export default function ExpandableCardDemo() {
+  const router = useRouter();
   const [cards, setCards] = useState([]);
+
+  const [isSubscribed, setIsSubscribed] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -16,6 +22,7 @@ export default function ExpandableCardDemo() {
         const fetchedCards = resp.data.meals.map((meal, index) => ({
           // description: resp.data.explanation,
           title: `${meal.meal_time}`,
+          show: !isSubscribed && index != 0 ? false : true,
           src: "https://assets.aceternity.com/demos/lana-del-rey.jpeg",
           ctaText: "show",
           ctaLink: "https://ui.aceternity.com/templates",
@@ -150,7 +157,14 @@ export default function ExpandableCardDemo() {
         <motion.div
           layoutId={`card-${card.title}-${id}`}
           key={`card-${card.title}-${id}`}
-          onClick={() => setActive(card)}
+          onClick={() => {
+            if(card.show){
+              setActive(card);
+            }
+            else{
+              router.push("/Pricing");
+            }
+          }}
           className="p-4 flex flex-col md:flex-row justify-between items-center hover:bg-neutral-50 dark:hover:bg-neutral-800 rounded-xl cursor-pointer">
           <div className="flex gap-4 flex-col md:flex-row ">
             <motion.div layoutId={`image-${card.title}-${id}`} className='flex justify-center items-center'>
@@ -176,8 +190,15 @@ export default function ExpandableCardDemo() {
           </div>
           <motion.button
             layoutId={`button-${card.title}-${id}`}
-            className="px-4 py-2 text-sm rounded-full font-bold bg-gray-100 hover:bg-green-500 hover:text-white text-black mt-4 md:mt-0">
-            {card.ctaText}
+            className={`${card.show ? "px-4 py-2 text-sm rounded-full font-bold bg-gray-100 hover:bg-green-500 hover:text-white text-black mt-4 md:mt-0" : ""}`}>
+              {
+                card.show ? 
+                <p>
+                  {card.ctaText}
+                </p>
+                :
+                <MdLockOutline />
+              }
           </motion.button>
         </motion.div>
       ))}
@@ -216,112 +237,3 @@ export const CloseIcon = () => {
     </motion.svg>)
   );
 };
-
-// const cards = [
-//   {
-//     description: "Lana Del Rey",
-//     title: "Summertime Sadness",
-//     src: "https://assets.aceternity.com/demos/lana-del-rey.jpeg",
-//     ctaText: "show",
-//     ctaLink: "https://ui.aceternity.com/templates",
-//     content: () => {
-//       return (
-//         (<p>Lana Del Rey, an iconic American singer-songwriter, is celebrated for
-//                     her melancholic and cinematic music style. Born Elizabeth Woolridge
-//                     Grant in New York City, she has captivated audiences worldwide with
-//                     her haunting voice and introspective lyrics. <br /> <br />Her songs
-//                     often explore themes of tragic romance, glamour, and melancholia,
-//                     drawing inspiration from both contemporary and vintage pop culture.
-//                     With a career that has seen numerous critically acclaimed albums, Lana
-//                     Del Rey has established herself as a unique and influential figure in
-//                     the music industry, earning a dedicated fan base and numerous
-//                     accolades.
-//                   </p>)
-//       );
-//     },
-//   },
-//   {
-//     description: "Babbu Maan",
-//     title: "Mitran Di Chhatri",
-//     src: "https://assets.aceternity.com/demos/babbu-maan.jpeg",
-//     ctaText: "show",
-//     ctaLink: "https://ui.aceternity.com/templates",
-//     content: () => {
-//       return (
-//         (<p>Babu Maan, a legendary Punjabi singer, is renowned for his soulful
-//                     voice and profound lyrics that resonate deeply with his audience. Born
-//                     in the village of Khant Maanpur in Punjab, India, he has become a
-//                     cultural icon in the Punjabi music industry. <br /> <br />His songs
-//                     often reflect the struggles and triumphs of everyday life, capturing
-//                     the essence of Punjabi culture and traditions. With a career spanning
-//                     over two decades, Babu Maan has released numerous hit albums and
-//                     singles that have garnered him a massive fan following both in India
-//                     and abroad.
-//                   </p>)
-//       );
-//     },
-//   },
-
-//   {
-//     description: "Metallica",
-//     title: "For Whom The Bell Tolls",
-//     src: "https://assets.aceternity.com/demos/metallica.jpeg",
-//     ctaText: "show",
-//     ctaLink: "https://ui.aceternity.com/templates",
-//     content: () => {
-//       return (
-//         (<p>Metallica, an iconic American heavy metal band, is renowned for their
-//                     powerful sound and intense performances that resonate deeply with
-//                     their audience. Formed in Los Angeles, California, they have become a
-//                     cultural icon in the heavy metal music industry. <br /> <br />Their
-//                     songs often reflect themes of aggression, social issues, and personal
-//                     struggles, capturing the essence of the heavy metal genre. With a
-//                     career spanning over four decades, Metallica has released numerous hit
-//                     albums and singles that have garnered them a massive fan following
-//                     both in the United States and abroad.
-//                   </p>)
-//       );
-//     },
-//   },
-//   {
-//     description: "Led Zeppelin",
-//     title: "Stairway To Heaven",
-//     src: "https://assets.aceternity.com/demos/led-zeppelin.jpeg",
-//     ctaText: "show",
-//     ctaLink: "https://ui.aceternity.com/templates",
-//     content: () => {
-//       return (
-//         (<p>Led Zeppelin, a legendary British rock band, is renowned for their
-//                     innovative sound and profound impact on the music industry. Formed in
-//                     London in 1968, they have become a cultural icon in the rock music
-//                     world. <br /> <br />Their songs often reflect a blend of blues, hard
-//                     rock, and folk music, capturing the essence of the 1970s rock era.
-//                     With a career spanning over a decade, Led Zeppelin has released
-//                     numerous hit albums and singles that have garnered them a massive fan
-//                     following both in the United Kingdom and abroad.
-//                   </p>)
-//       );
-//     },
-//   },
-//   {
-//     description: "Mustafa Zahid",
-//     title: "Toh Phir Aao",
-//     src: "https://assets.aceternity.com/demos/toh-phir-aao.jpeg",
-//     ctaText: "show",
-//     ctaLink: "https://ui.aceternity.com/templates",
-//     content: () => {
-//       return (
-//         (<p>Aawarapan, a Bollywood movie starring Emraan Hashmi, is
-//                     renowned for its intense storyline and powerful performances. Directed
-//                     by Mohit Suri, the film has become a significant work in the Indian
-//                     film industry. <br /> <br />The movie explores themes of love,
-//                     redemption, and sacrifice, capturing the essence of human emotions and
-//                     relationships. With a gripping narrative and memorable music,
-//                     Aawarapan has garnered a massive fan following both in
-//                     India and abroad, solidifying Emraan Hashmi`&apos;`s status as a
-//                     versatile actor.
-//                   </p>)
-//       );
-//     },
-//   },
-// ];
